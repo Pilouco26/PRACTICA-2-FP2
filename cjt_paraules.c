83a56d426e0c44c363b386df17614decd4850685
Douuniia22 <102051122+Douuniia22@users.noreply.github.com>
2022-05-09 09:03:34 +0200

    Update cjt_paraules.c

--- cjt_paraules.c
+++ cjt_paraules.c
@@ -6,7 +6,7 @@
 
 void inicialitzar(cjt_paraules_t *c)
 {
-    c->n_el=0; //incialitza //FET 100%
+    c->n_el=0; //incialitza el conjunt amb zeros
 }
 bool pertany(cjt_paraules_t *c, char p[])
 {
@@ -14,6 +14,7 @@ bool pertany(cjt_paraules_t *c, char p[])
     bool r;
     r=false;
 
+    //Bucle per mirar si la paraula ja pertany al conjunt, amb un strcmp si les paraules son iguals retorna 0 i true.
     for(i=0; i< (c->n_el); i++)
     {
          if(strcmp(c->paraules[i], p)==0) //si son iguals retorna 0
@@ -22,10 +23,8 @@ bool pertany(cjt_paraules_t *c, char p[])
         }
     }
     return r;
-    //problemas chicos, error de novato
 }
-
-
+// Esta ple si el numero d'elements coincideix amb el maxim
 bool ple(cjt_paraules_t *c)
 {
     bool r;
@@ -36,79 +35,80 @@ bool ple(cjt_paraules_t *c)
     }
     return r;
 }
+//Si el num d'elements es 0 retorna true
 bool buit(cjt_paraules_t *c)
 {
     bool r;
     r=false;
-    if(c->n_el==0)     //SI no hi ha cap element esta buit   //FET 100%
+    if(c->n_el==0)     //Si no hi ha cap element esta buit
     {
-          r=true; //indica que es veritat
+          r=true;
     }
-    return r; //envia el resultat
-    //S'ha enganchao, s'ha enganchao, s'ha enganchao; en la puzolana! ¡Ahí está el milagro!
+    return r;
 }
+
 bool afegir(cjt_paraules_t *c, char p[])
 {
 
     int i;
-
     bool r =true;
-    i=0;
-        while (i<(c->n_el+1) && (r!=false) )
+    n_elems(c); //obtenim num d'elements
+
+        //bucle per mirar que les lletres siguin minuscules o numeros
+        i=0;
+        while (i<(c->n_el+1) && (r!=false) ) //fa el bucle mentre i sigui menor que el num d'elements mes 1 i mentre sigui true.
         {
-              if ((((int)p[i]<48) && ((int)p[i]>57)) || (((int)p[i] <97) && ((int)p[i] >122)))
+              if ((((int)p[i]<48) && ((int)p[i]>57)) || (((int)p[i] <97) && ((int)p[i] >122))) //comprova comparant amb el codi ascii
                 {
-                    r= false;
+                    r= false;//si es compleix una de les condicions retorna fals
                 }
-            i++;
+                i++;
         }
 
-            n_elems(c);
-             if(ple(c)||(strlen(p)>=8)|| pertany(c, p)) r=false;
+            if(ple(c)||(strlen(p)>=8)|| pertany(c, p)) r=false; //si esta ple o la paraula es mes llarga del indicat o ia pertany retorna false
             else
                 {
                     c->paraules[c->n_el]=(char*)malloc(sizeof(char)*(strlen(p)+1)); //obra l'espai per memoria dinamica char * cadena + 1
                     strcpy(c->paraules[c->n_el], p); //copia la paraula a la cadena
                     c->n_el++; //suma elements
                 }
-
     return r;
-    //falta corregir + aplicar els caracters predeterminats per l'enunciat.
+
 }
 void eliminar(cjt_paraules_t *c, char p[])
 {
-    int i, posicio, comparacio;
+    int i, posicio;
     n_elems(c); //reb els elements que hi ha
 
-    if (!buit(c) && pertany(c, p))
+    if (!buit(c) && pertany(c, p)) // si no esta buit i pertany
     {
-
         i=0;
-        posicio=0;
-    	while(strcmp(c->paraules[i], p)!=0)
+        posicio=0; // variable per obtenir la posicio a on esta.
+    	while(strcmp(c->paraules[i], p)!=0)  //mentre les paraules siguin diferents fa el bucle
     	{
-         	if(strcmp(c->paraules[i], p)==0) //si son iguals retorna 0
+         	if(strcmp(c->paraules[i], p)==0) //quan siguin iguals guardem la posicio.
         	{
                 posicio=i; //posicio de la paraula a eliminar
         	}
         	i++;
     	}
-    	free(c->paraules[i]);
+    	free(c->paraules[c->n_el]); //eliminem la paraula
     	c->n_el--;
-    	//borra la paraula
+    	//borra la paraula i restem un element
 
     	for(i=posicio; i<(c->n_el); i++)
     	{
-        	c->paraules[i]= c->paraules[i+1];
+        	c->paraules[i]= c->paraules[i+1]; //movem tot una posicio enrere
     	}
 	}
-     //Grans avanços
-    //problamement ya esta.
+
 }
+//obtenim numero d'elements
 int n_elems(cjt_paraules_t *c)
 {
     return(c->n_el);
 }
+//procediment per mostra
 void mostrar(cjt_paraules_t *c)
 {
     for(int i=0; i<c->n_el; i++)
@@ -117,5 +117,4 @@ void mostrar(cjt_paraules_t *c)
         printf("\n");
     }
 
-    //Falla algo amb  mostar
 }
